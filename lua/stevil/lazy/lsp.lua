7837850889ca2a56fdbbf0ddde921e0c62bd6fbc
Step <stepan.v.latkin@gmail.com>
2025-06-27 15:32:48 +0300

    LSP works now for c and python

--- lua/stevil/lazy/lsp.lua
+++ lua/stevil/lazy/lsp.lua
@@ -16,6 +16,7 @@ return {
   },
 
   config = function()
+    local cmp = require('cmp')
     local lspconfig = require("lspconfig")
     local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     --local mason = require("mason")
@@ -69,6 +70,40 @@ return {
             "clangd",
         }
     }
+
     -- ...
+
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            end,
+        },
+        mapping = cmp.mapping.preset.insert({
+            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+            ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+            { name = "copilot", group_index = 2 },
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' }, -- For luasnip users.
+        }, {
+            { name = 'buffer' },
+        })
+    })
+
+    vim.diagnostic.config({
+        -- update_in_insert = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = true,
+            header = "",
+            prefix = "",
+        },
+    })
   end,
 }
