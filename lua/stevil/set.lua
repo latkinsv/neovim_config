5f6399d79f6af550be81a52fc553b7099cc9ab85
Step <stepan.v.latkin@gmail.com>
2026-01-20 21:31:25 +0300

    turn on wrap

--- lua/stevil/set.lua
+++ lua/stevil/set.lua
@@ -9,10 +9,9 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-
 opt.smartindent = true
 
-opt.wrap = false
+opt.wrap = true
 
 opt.swapfile = false
 opt.backup = false
